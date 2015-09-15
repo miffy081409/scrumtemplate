276b7e16e454b08a154404da0e8a87c339ed32e3
Programmer Karl <miffy081409@gmail.com>
2015-09-15 18:47:22 +0800

    update

---
+++ DeveloperAPI/Controllers/DeveloperAPIController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DeveloperAPI.Controllers
+{
+    public class DeveloperAPIController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Register()//so that we can have uniform view and no need to create new page every time we add new api
+        {
+            return View();
+        }
+    }
+}
