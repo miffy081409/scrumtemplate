830f4d8ac20f0b676d319dbc825decd61a29e6aa
miffy081409 <miffy081409@gmail.com>
2015-09-18 16:44:52 +0800

    [update] make angular view model testable

---
+++ DeveloperAPI/wwwroot/apps/Search/Models/api.js
@@ -0,0 +1,19 @@
+﻿var searchAppModule = window.searchApp;
+
+searchAppModule.service('apiModel', function () {
+
+    var apiArray = new Array();
+
+    var testData = function () {
+        for (var ctr = 1; ctr <= 10; ctr++) {
+            apiArray[ctr - 1] = { ID: ctr, Title: 'API Name ' + ctr, Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum' };
+        }
+
+        return apiArray;
+    };
+
+    this.getData = function ()
+    {
+        return testData();
+    };
+});
--- DeveloperAPI/wwwroot/apps/Search/ViewModels/searchViewModel.js
+++ DeveloperAPI/wwwroot/apps/Search/ViewModels/searchViewModel.js
@@ -1,17 +1,12 @@
 ﻿var searchAppModule = window.searchApp;
 
-searchAppModule.controller("searchViewModel", function ($scope, $http, $location) {
+searchAppModule.controller("searchViewModel", function ($scope, $http, $location, apiModel) {
     
-    var origList = new Array();
+    var origList = apiModel.getData();//get data from service
     
     $scope.searchKeyword = '';
     $scope.searchTitle = '';
     $scope.results = new Array();
-
-    //test data only
-    for (var ctr = 1; ctr <= 10; ctr++) {
-        origList[ctr - 1] = { ID: ctr, Title: 'API Name ' + ctr, Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum' };
-    }
 
     $scope.keywordChanged = function ()
     {
@@ -26,7 +21,12 @@ searchAppModule.controller("searchViewModel", function ($scope, $http, $location
                 }
             }, result);
 
-            $scope.searchTitle = 'Search for api keyword "' + $scope.searchKeyword + '"';
+            if (result.length > 0) {
+                $scope.searchTitle = 'Search for api keyword "' + $scope.searchKeyword + '"';
+            }
+            else {
+                $scope.searchTitle = 'No result found for api keyword "' + $scope.searchKeyword + '"';
+            }
             $scope.results = result;
         }
         else {
