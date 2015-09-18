bd4b9ecf84fe0fe99eb7f29bd90d950f0d84cef3
Programmer Karl <miffy081409@gmail.com>
2015-09-18 22:46:35 +0800

    update

---
+++ DeveloperAPI/wwwroot/apps/API/apiApp.js
@@ -0,0 +1,19 @@
+﻿window.apiApp = angular.module('apiApp', [
+        // Angular modules 
+        'ngRoute'
+
+        // Custom modules 
+
+        // 3rd Party Modules
+
+])
+.config(function ($routeProvider, $locationProvider) {
+    //configure angular routing here
+    $routeProvider.when('/api/register', { templateUrl: '/apps/API/View/registration.html', controller: 'registrationViewModel' })
+        .when('/api/update', { templateUrl: '/apps/API/View/registration.html', controller: 'registrationViewModel' })
+        .otherwise({ redirectTo: '/' });
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+});
